5cbd44664581c84f1a7a25c04bed50382934a890
Carlos Pizarro <pizarro.c.barboza@outlook.com>
2026-08-22 02:11:35 -0600

    Pydantic Models and BaseModel basics. Exercise 1.

---
+++ src/exercises.py
@@ -0,0 +1,44 @@
+"""
+Exercise 1: Building your first Model
+
+Scenario:
+You are building an ingestion script for a vulnerability scanner. 
+The scanner outputs a flat dictionary for each finding. You need to model this data.
+
+Your Task:
+Write a Pydantic BaseModel named VulnerabilityFinding that includes the following fields:
+
+    cve_id: A string representing the vulnerability ID (e.g., "CVE-2026-1234").
+
+    cvss_score: A float representing the severity score.
+
+    patched: A boolean indicating if a patch has been applied.
+
+Test Data:
+Imagine your pipeline receives this messy dictionary from the scanner API:
+Python
+
+api_response = {
+    "cve_id": "CVE-2026-9999",
+    "cvss_score": "8.5",  # Note: It's a string!
+    "patched": "False"    # Note: It's a string!
+}
+
+Task: Show me the class definition, and show me how you would instantiate it using the api_response dictionary.
+"""
+from pydantic import BaseModel
+
+class VulnerabilityFinding(BaseModel):
+    cve_id: str
+    cvss_score: float
+    patched: bool
+
+api_response: dict[str, str] = {
+    "cve_id": "CVE-2026-9999",
+    "cvss_score": "8.5",  # Note: It's a string!
+    "patched": "False"    # Note: It's a string!
+}
+
+log: VulnerabilityFinding = VulnerabilityFinding(**api_response)
+
+print(log)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,55 @@
+from pydantic import BaseModel
+from typing import Optional
+
+# Default: if not passed in, set to some value
+# Optional: Effectively the same as default, but we can pass None!
+class User(BaseModel):
+    """A class to represent a simple user.
+
+    Args:
+        user (str): name of the user
+        age (int): age of the user
+    """
+    name: str
+    age: int
+    pass_induction: bool
+    # years_experience: int = 0
+    # years_experience: Optional[int] = 0
+    years_experience: int | float | str
+    awards: list[str]
+
+class ServerConfig(BaseModel):
+    hostname: str
+    ram_gb: int
+    is_active: bool
 
 
 def main() -> None:
-    ...
+    user = User(
+        name="Charlie", 
+        age="29", 
+        pass_induction=True, 
+        years_experience="1 week", 
+        awards=["AWS Cert", "Terraform Cert"]
+    )
+
+    # print(user)
+
+    server1 = ServerConfig(
+        hostname="db-prod-01",
+        ram_gb=64,
+        is_active=True
+    )
+
+    print(server1.ram_gb) # I can use dot notation to access my model's fields
+
+    # Coercion (Parsing in action)
+    raw_data: dict[str, str] = {"hostname": "cache-01", "ram_gb": "32", "is_active": "true"}
+    server2 = ServerConfig(**raw_data)
+
+    # Pydantic coerced the strings into the correct Python types
+    print(repr(server2.ram_gb))
+    print(repr(server2.is_active))
 
 if __name__ == "__main__":
     main()
