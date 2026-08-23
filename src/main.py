2a02cb8cde3ad7e75f01e1897b7d747cac6a855c
Carlos Pizarro <pizarro.c.barboza@outlook.com>
2026-08-22 19:52:25 -0600

    Pydantic basic field validation. Added pytest to the mix.

--- src/main.py
+++ src/main.py
@@ -1,55 +1,32 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, field_validator
 
-# Default: if not passed in, set to some value
-# Optional: Effectively the same as default, but we can pass None!
-class User(BaseModel):
-    """A class to represent a simple user.
+"""--- Pydantic - Basic Validators ---"""
 
-    Args:
-        user (str): name of the user
-        age (int): age of the user
-    """
+# Why validate?
+# Setting up a validator to check a field
+# Modifying values
+# Reusing validators
+
+class Person(BaseModel):
+    """A class represent a Person"""
     name: str
     age: int
-    pass_induction: bool
-    # years_experience: int = 0
-    # years_experience: Optional[int] = 0
-    years_experience: int | float | str
-    awards: list[str]
 
-class ServerConfig(BaseModel):
-    hostname: str
-    ram_gb: int
-    is_active: bool
+    @field_validator("name")
+    def check_name_is_alpha(word: str) -> str:
+        if not word.isalpha():
+            raise ValueError(f"{word} isn't correctly formed!")
+        return word
 
+    @field_validator("age")
+    def check_correct_age(age: int) -> int:
+        if age < 1 or age > 100:
+            raise ValueError(f"{age} isn't correctly formed!")
+        return age
 
 def main() -> None:
-    user = User(
-        name="Charlie", 
-        age="29", # type: ignore 
-        pass_induction=True, 
-        years_experience="1 week", 
-        awards=["AWS Cert", "Terraform Cert"]
-    )
-
-    # print(user)
-
-    server1 = ServerConfig(
-        hostname="db-prod-01",
-        ram_gb=64,
-        is_active=True
-    )
-
-    print(server1.ram_gb) # I can use dot notation to access my model's fields
-
-    # Coercion (Parsing in action)
-    raw_data: dict[str, str] = {"hostname": "cache-01", "ram_gb": "32", "is_active": "true"}
-    server2 = ServerConfig(**raw_data) # type: ignore
-
-    # Pydantic coerced the strings into the correct Python types
-    print(repr(server2.ram_gb))
-    print(repr(server2.is_active))
+    charlie = Person(name="Charlie", age=29)
+    print(charlie)
 
 if __name__ == "__main__":
     main()
---
+++ tests/pytest_test.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_assert_true() -> None:
+    assert True
+
+def test_assert_false() -> None:
+    assert False
